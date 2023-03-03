6f9aa6e625b36f4d34b1f4fedf567e93f92190a7
Dusan <dusanstojanovic256@gmail.com>
2023-03-03 12:26:31 +0100

    fixed cache

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,22 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 type FetchResult = {
-  price: number;
-  time: Date;
+    price: number;
+    time: Date;
 }[];
 type LargestTimeDifference = {
-  largestDifference: number;
-  item1: Date;
-  item2: Date;
+    largestDifference: number;
+    item1: Date;
+    item2: Date;
 };
 type FetchFunction = (id: number) => Promise<FetchResult>;
-
-declare function createHandler(
-  ttl: number,
-  fetchFunc: FetchFunction
-): (req: Request, res: Response, next: NextFunction) => Promise<void>;
+declare function createHandler(ttl: number, fetchFunc: FetchFunction): (req: Request, res: Response) => Promise<void>;
 export declare const getFilteredArray: (arr: FetchResult) => FetchResult;
 export declare const sortByTime: (arr: FetchResult) => FetchResult;
-export declare const findLargestTimeDifference: (
-  arr: FetchResult
-) => LargestTimeDifference;
+export declare const findLargestTimeDifference: (arr: FetchResult) => LargestTimeDifference;
 export { createHandler };
